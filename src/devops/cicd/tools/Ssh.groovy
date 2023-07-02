c0228d884826e2121946830ccfb35a396499dc6f
daotoan <external.toan.dao@sky.de>
2023-07-02 14:55:40 +0700

    update

--- src/devops/cicd/tools/Ssh.groovy
+++ src/devops/cicd/tools/Ssh.groovy
@@ -1,19 +1,22 @@
 package devops.cicd.tools
 
+import groovy.transform.Field
 
+@Field SSH_KEY_PREPARED = false
 
 def prepare(credential = "ssh-key") {
+    if (SSH_KEY_PREPARED) {
+        return
+    }
     withCredentials([file(credentialsId: credential, variable: 'FILE')]) {
         def text = readFile(FILE)
         writeFile(file: 'ssh-key', text: text)
         sh "chmod 400 ssh-key"
     }
+    SSH_KEY_PREPARED = true
 }
 
 def executeCommand(hostname, command, username = "cicd") {
     prepare()
-    return sh(
-        script: "ssh -i ssh-key -o StrictHostKeyChecking=no ${username}@${hostname} -- ${command}",
-        returnStdout: true
-    )
+    sh "ssh -i ssh-key -o StrictHostKeyChecking=no ${username}@${hostname} -- ${command}"
 }
