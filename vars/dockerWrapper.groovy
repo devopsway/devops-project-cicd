2aede2b99612ddf284f4c269e601a39f1c324ea4
daotoan <external.toan.dao@sky.de>
2023-07-02 14:59:17 +0700

    update

--- vars/dockerWrapper.groovy
+++ vars/dockerWrapper.groovy
@@ -2,26 +2,24 @@ import devops.cicd.tools.Ssh
 
 def call() {
     dockerNode(image: 'daotoanhd/devopstools:1.0.0') {
-        stage("test") {
-            def ssh = new Ssh()
-            def serverIP = "34.126.122.163"
-            def applicationDir = "/home/vagrant/devops-project-application-cowsay-frontend"
-            stage("Pull Code") {
-                ssh.executeCommand(serverIP, executeDir(applicationDir, "git pull"), "vagrant")
-            }
-            stage("Install Dependencies") {
-                ssh.executeCommand(serverIP, executeDir(applicationDir, "npm install"), "vagrant")
-            }
-            stage("Build application") {
-                ssh.executeCommand(serverIP, executeDir(applicationDir, "npm build"), "vagrant")
-            }
-            stage("Restart application") {
-                ssh.executeCommand(serverIP, executeDir(applicationDir, "pm2 restart frontend"), "vagrant")
-            }
+        def ssh = new Ssh()
+        def serverIP = "34.126.122.163"
+        def applicationDir = "/home/vagrant/devops-project-application-cowsay-frontend"
+        stage("Pull Code") {
+            ssh.executeCommand(serverIP, executeDir(applicationDir, "git pull"), "vagrant")
+        }
+        stage("Install Dependencies") {
+            ssh.executeCommand(serverIP, executeDir(applicationDir, "npm install"), "vagrant")
+        }
+        stage("Build application") {
+            ssh.executeCommand(serverIP, executeDir(applicationDir, "npm build"), "vagrant")
+        }
+        stage("Restart application") {
+            ssh.executeCommand(serverIP, executeDir(applicationDir, "pm2 restart frontend"), "vagrant")
         }
     }
 }
 
 def executeDir(dir, command) {
-    return "cd ${dir} && ${command}"
+    return "'cd ${dir} && ${command}'"
 }
